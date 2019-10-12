86ac5531c32575ce4e6a26505e10450163028a5e
Toshio <felipetoshio1214@gmail.com>
2019-10-12 14:07:38 -0300

    Refactor to new class

--- Program.cs
+++ Program.cs
@@ -1,34 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using TrabalhoMarcia.src;
 
 namespace TrabalhoMarcia
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string[] expressao = new string[50];
-
-            Console.WriteLine("Para Zmax, digite 1, para Zmin digite 2:");
-            var Ztipo = Console.ReadLine();
-
-            Console.WriteLine("Digite a expressão de Z:");
-            var Z = Console.ReadLine();
-
-            for (int i = 0; i <= expressao.Length; i++)
-            {
-                Console.WriteLine("Digite expressão limitante:");
-                expressao[i] = Console.ReadLine();
-                Console.WriteLine(expressao[i]);
-
-                Console.WriteLine("Se não quiser escrever outra expressao, digite falso, caso queira digite qualquer valor:");
-                var aux = Console.ReadLine();
-                if
-                    (aux == "falso")
-                {
-                    break;
-                }
-            }
+            Operations.GetTypeZ();
+            Operations.GetZ();
+            Operations.SetLimitantExpressions();
         }
     }
 }
---
+++ src/Operations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabalhoMarcia.src
+{
+    public class Operations
+    {
+        public static string GetTypeZ()
+        {
+            Console.WriteLine("Para Zmax, digite 1, para Zmin digite 2:");
+            var Ztipo = Console.ReadLine();
+            return Ztipo;
+        }
+
+        public static string GetZ()
+        {
+            Console.WriteLine("Digite a expressão de Z:");
+            var Z = Console.ReadLine();
+            return Z;
+        }
+
+        public static void SetLimitantExpressions()
+        {
+            string[] expressao = new string[50];
+
+            for (int i = 0; i <= expressao.Length; i++)
+            {
+                Console.WriteLine("Digite expressão limitante:");
+                expressao[i] = Console.ReadLine();
+
+                Console.WriteLine("Se não quiser escrever outra expressao, digite falso, caso queira digite qualquer valor:");
+                var aux = Console.ReadLine();
+                if (aux == "falso")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
