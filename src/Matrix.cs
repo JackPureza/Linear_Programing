f698ab0bd0f7da91802d87d461f35461891efdb0
João Vitor Pureza <pureza.contato@outlook.com>
2019-10-13 11:38:36 -0300

    Initial commit

---
+++ src/Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text0;
+using System.Text.RegularExpressions;
+using TrabalhoMarcia.src;
+
+namespace TrabalhoMarcia.src
+{
+    public class Matrix
+    {
+        private Operations operations;
+
+        public Matrix(Operations operations)
+        {
+            this.operations = operations;
+        }
+
+        public void resolveMatrix()
+        {
+
+        }
+
+        public bool isSimplex(Operations operations, var type)
+        {
+            bool simplex;
+
+            if (type == "1")
+            {
+                simplex == true;
+            }
+            else
+            {
+                simplex == false;
+            }
+
+            return simplex;
+        }
+    }
+}
