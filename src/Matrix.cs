138fb251c8136e118bbe1cb135de9c1268a346cc
João Vitor Pureza <pureza.contato@outlook.com>
2019-10-13 23:23:45 -0300

    refatorando matrix

--- src/Matrix.cs
+++ src/Matrix.cs
@@ -29,7 +29,7 @@ namespace TrabalhoMarcia.src
             return simplex;
         }
 
-        public static void PrintMatrix(int?[,] matrix)
+        public static void PrintMatrix(double?[,] matrix)
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
@@ -45,36 +45,110 @@ namespace TrabalhoMarcia.src
             }
         }
 
-        public static int?[,] SimplexResolve(int?[,] matrix)
+        public static double?[,] SimplexResolve(double?[,] matrix)
         {
-            ProductionProcess(matrix);
-            
+            bool verification = true;
+
+            while (verification)
+            {
+                int chosenColumn = GetChosenColumn(matrix);
+                int chosenLine = ProductionProcess(matrix);
+
+                double[,] matrixAux = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+
+                double pivo = matrix[chosenLine, chosenColumn] / matrix[chosenLine, chosenColumn] ?? default(int);
+
+                matrixAux[chosenLine, chosenColumn] = pivo;
+
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    if (i != chosenColumn)
+                    {
+                        matrixAux[chosenLine, i] = matrix[chosenLine, i] / matrix[chosenLine, chosenColumn] ?? default(int);
+                    }
+                }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    chosenLine++;
+                    if (chosenLine >= matrix.GetLength(0))
+                    {
+                        chosenLine = 0;
+                    }
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrixAux[chosenLine, j] = (matrix[chosenLine, j] - matrix[chosenLine, chosenColumn]) * pivo ?? default(int);
+                    }
+                }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrixAux[i, j];
+                    }
+
+                }
+                PrintMatrix(matrix);
+                verification = VerifyLastLine(matrix);
+            }
+            return matrix;
         }
 
-        public static int? ProductionProcess(int?[,] matrix) {
+        public static int ProductionProcess(double?[,] matrix)
+        {
 
-            int? comparator = matrix[matrix.GetLength(0), 1];
+            int chosenColumn = GetChosenColumn(matrix);
+            int line = 0;
+            double? chosenLine = 999999999;
+            double? comp;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i,chosenColumn] == 0)
+                {
+                    comp = -1;
+                }
+                else
+                {
+                    comp = matrix[i, matrix.GetLength(1) - 1] / matrix[i, chosenColumn];
+                }
+
+                if (comp < chosenLine && comp >= 0)
+                {
+                    chosenLine = comp;
+                    line = i;
+                }
+            }
+
+            return line;
+        }
+
+        public static int GetChosenColumn(double?[,] matrix)
+        {
+            double? comparator = matrix[matrix.GetLength(0) - 1, 1];
             int chosenColumn = 0;
-            int? chosenLine = 0;
+
             for (int i = 0; i < matrix.GetLength(1); i++)
             {
-                if (matrix[matrix.GetLength(0), i] < comparator)
+                if (matrix[matrix.GetLength(0) - 1, i] < comparator)
                 {
-                    comparator = matrix[matrix.GetLength(0), i];
+                    comparator = matrix[matrix.GetLength(0) - 1, i];
                     chosenColumn = i;
                 }
             }
+            return chosenColumn;
+        }
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+        public static bool VerifyLastLine(double?[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
             {
-                int? comp = matrix[i, matrix.GetLength(1)] / matrix[i, chosenColumn];
-
-                if (comp < chosenLine)
-                {
-                    chosenLine = comp;
-                }
+                if (matrix[matrix.GetLength(0) - 1, i] < 0)
+                    return false;
+                return true;
             }
-            return chosenLine;
+            return true;
         }
     }
 }
